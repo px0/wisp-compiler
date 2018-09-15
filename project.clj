f7411a878efef8fbd4049878906fbf37687fe348
Max Gerlach <m@px0.de>
2018-09-15 16:52:09 -0400

    set repo url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject wisp-compiler "0.1.0-SNAPSHOT"
   :description "Compile wisp expressions to JavaScript in your Clojure project!"
-  :url ""
+  :url "https://github.com/px0/wisp-compiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
