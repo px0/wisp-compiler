a8d0c59aebcd54e47637eacc33f1bc6dff91f430
Max Gerlach <m@px0.de>
2018-09-15 19:28:57 -0400

    inc version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wisp-compiler "0.2.0-SNAPSHOT"
+(defproject wisp-compiler "0.2.1-SNAPSHOT"
   :description "Compile wisp expressions to JavaScript in your Clojure project!"
   :url "https://github.com/px0/wisp-compiler"
   :license {:name "Eclipse Public License"
