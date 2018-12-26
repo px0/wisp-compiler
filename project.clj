d71b6f3899766ebb14cd43b2ab54b20912a0f69c
Max Gerlach <m@px0.de>
2018-12-26 10:25:19 -0500

    typo

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/px0/wisp-compiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  deploy-repositories [["clojars"  {:sign-releases false :url "https://clojars.org/repo"}]]
+  :deploy-repositories [["clojars"  {:sign-releases false
+                                    :url "https://clojars.org/repo"}]]
   :dependencies [[org.clojure/clojure "1.8.0"]])
