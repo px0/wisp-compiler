fedb4f31c90904164d6f8742e1d6ae3285122df0
Max Gerlach <m@px0.de>
2018-12-28 21:34:03 -0500

    trying to release

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/px0/wisp-compiler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :deploy-repositories [["clojars"  {:sign-releases false
-                                    :url "https://clojars.org/repo"}]]
+  ;; :deploy-repositories [["clojars"  {:sign-releases false
+  ;;                                    :url "https://clojars.org/repo"}]]
+  :deploy-repositories {"releases" {:url "https://repo.clojars.org" :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]])
